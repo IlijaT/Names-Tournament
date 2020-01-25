1cb2acb721768c33614f26d8afff4f09c8748c9c
Ilija Tatalovic <tatalovicilija@gmail.com>
2020-01-25 13:59:03 +0100

    Model many to many relations

--- app/Tournament.php
+++ app/Tournament.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Tournament extends Model
 {
     protected $guarded = [];
+
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tournaments()
+    {
+        return $this->belongsToMany('App\Tournament', 'tournament_user');
+    }
 }
