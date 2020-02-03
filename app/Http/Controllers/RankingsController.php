061c058dfbcd38d7dea08eae53f6573c720439fa
Ilija Tatalovic <tatalovicilija@gmail.com>
2020-02-03 22:51:41 +0100

    ranking controller test

--- app/Http/Controllers/RankingsController.php
+++ app/Http/Controllers/RankingsController.php
@@ -9,6 +9,7 @@ class RankingsController extends Controller
 {
     public function index()
     {
+        dd('u ranking kontroloru ');
         // $users = User::orderBy('points', 'desc')->paginate(50);
         return view('rankings');
     }
